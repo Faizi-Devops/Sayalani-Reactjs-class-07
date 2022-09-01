2fc6fc0e853defba05c66d0194ebcad0c6f7de0e
Shahzada-Faizi <devfaizi435@gmail.com>
2022-09-01 04:12:53 -0700

    add toastify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 import './App.css';
 import Navbar from './components/Navbar';
 import Routing from "./Routing/Routing";
@@ -8,6 +9,22 @@ function App() {
   return (
     <div>
       <Routing />
+
+      <ToastContainer
+position="bottom-center"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
+
+
+
+
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,34 @@
+import {  toast } from 'react-toastify';
 let About = () =>{
+    let onctaError = () =>{
+        toast.error('🦄 Data Has been Added to the record!', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+
+    }
+    let onctaSuccess = () =>{
+
+        toast.success('🦄 Wow you are success!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+    }
     return(
         <div>
             <h1>This is a About Component</h1>
+            <button type="button" className='btn btn-danger' onClick={onctaError}>Error</button>
+            <button type='button' className='btn btn-primary' onClick={onctaSuccess}>Success</button>
 
         </div>
     )
